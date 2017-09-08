134c3840a5c20ff31e782a1effb36d4f30da56fc
Alexandr Zadyryaka <sashok@se7ensky.com>
2017-09-08 10:50:10 +0300

    fix deploy script

--- deploy2dokku.sh
+++ deploy2dokku.sh
@@ -3,7 +3,7 @@
 PROJECT_NAME=btbeverage
 OUT_FOLDER=dist
 
-npm install ; yarn build:min
+yarn ; yarn build:min
 rm -rf tmp ; git clone --depth 1 https://github.com/SE7ENSKY/heroku-express-template.git tmp
 ( cd tmp ; rm -rf .git ; rm -rf public )
 cp -r $OUT_FOLDER tmp/public

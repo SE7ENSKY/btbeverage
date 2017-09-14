3b843cf394122192930718dfda31a9e9a4b29a11
Alexandr Zadyryaka <sashok@se7ensky.com>
2017-09-14 15:53:50 +0300

    add modals, cart modal

--- src/assets/btbeverage.js
+++ src/assets/btbeverage.js
@@ -20,6 +20,8 @@ import 'styles/main.styl';
 
 // ------------------ import vendor scripts ------------------
 import 'vendor/modernizr.js';
+import 'vendor/twitter-bootstrap/transition.js';
+import 'vendor/twitter-bootstrap/modal.js';
 import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 
